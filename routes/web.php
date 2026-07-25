546f68a40cb5e3826af66754d005291c4c39b5c1
Riska <riskafebriana168@gmail.com>
2026-07-25 10:43:04 +0700

    Create employee model and migration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [DashboardController::class, 'index']);
+Route::resource('employees', EmployeeController::class);
